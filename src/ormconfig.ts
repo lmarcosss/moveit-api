cc6769f7d5ff8154fdae570b2be54d77980c836f
Leonardo Marcos <leonardo.marcos@cwi.com.br>
2021-03-21 21:55:25 -0300

    comment ormconfig

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -3,13 +3,13 @@ import { ConnectionOptions } from "typeorm";
 export default {
     "type": "postgres",
     "url": process.env.DATABASE_URL,
-    "entities": [process.env.ENTITIES || ''],
-    "migrations": [process.env.MIGRATIONS || ''],
+    "entities": [process.env.ENTITIES],
+    "migrations": [process.env.MIGRATIONS],
     "cli": {
         "migrationsDir": "./src/database/migrations"
     },
-    synchronize: false,
-    logging: true,
+    // synchronize: false,
+    // logging: true,
     // extra: {
     //     ssl: {
     //         rejectUnauthorized: false,
